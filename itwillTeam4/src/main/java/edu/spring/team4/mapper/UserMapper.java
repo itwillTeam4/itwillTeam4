b58c17323570ebe3773f74116323adc30b8d3e45
YIREUM <YIREUM@192.168.2.12>
2022-02-14 17:30:57 +0900

    usermapper.java 수정중

--- itwillTeam4/src/main/java/edu/spring/team4/mapper/UserMapper.java
+++ itwillTeam4/src/main/java/edu/spring/team4/mapper/UserMapper.java
@@ -11,12 +11,11 @@ public interface UserMapper {
 
 	
 	String SIGN_UP = 
-			"INSERT INTO ${TABLE_USERS} (${COL_USERID}, ${COL_PWD}, ${COL_EMAIL}) "
-			+ "VALUES (#{userid}, #{pwd}, #{email})";
+			"INSERT INTO ${TABLE_USERS} (${COL_USER_ID}, ${COL_USER_PWD}, ${COL_USER_NN}, ${COL_USER_NAME}, ${COL_USER_PHONE}) "
+			+ "VALUES (#{user_id}, #{user_pwd}, #{user_nn}, #{user_name}, #{user_phone})";
 	
 	@Insert(SIGN_UP)
 	int insert(User user);
-	
 	
 	String CHECK_USER_ID = 
 			"SELECT * FROM ${TABLE_USERS} WHERE ${COL_USERID} = #{userid}";
